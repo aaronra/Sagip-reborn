c934855cd8da6b1c9c59c868811ce51351990dcd
Jr Reyes <jr@internetsoftwaresystems.com>
2015-08-18 15:23:16 +0800

    added toast

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+    name: 'Sagip Reborn',
+    description: 'one click sagip',
+    author: 'Code Jaeger',
+    website: 'http://localhost:3000'
+});
+
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
